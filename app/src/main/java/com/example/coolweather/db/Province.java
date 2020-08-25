89532808e6f7dd20ab2cfe55b7998fb11788b261
qixiangbo <1986234350@qq.com>
2020-08-25 19:06:33 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,27 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport { //继承DataSupport类
+    private int id;
+    private  String provinceName;   //省的名字
+    private int provinceCode;   //省的代号
+    public int getId(){
+        return id;
+    }
+    public  void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
